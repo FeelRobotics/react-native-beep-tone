31ea144c16d41cb4550adbce39047453c41002f5
Alexey Korepanov <alexkorep@gmail.com>
2019-07-17 10:23:42 +0200

    Fix iOS implementation

--- ios/RNBeepTone.m
+++ ios/RNBeepTone.m
@@ -1,19 +1,29 @@
 #import "RNBeepTone.h"
+#import "TGSineWaveToneGenerator.h"
 
 @implementation RNBeepTone
 
+TGSineWaveToneGenerator *generator = NULL;
+
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(start:(NSNumber *)frequency numberParameter:(nonnull NSNumber *)volume)
+RCT_EXPORT_METHOD(start:(nonnull NSNumber *)volume)
 {
-    // TODO: Implement some real useful functionality
-    //callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+    if (generator) {
+        [generator stop];
+        generator = NULL;
+    }
+
+    float amplitude = [volume doubleValue]*SINE_WAVE_TONE_GENERATOR_AMPLITUDE_FULL/100;
+    generator = [[TGSineWaveToneGenerator alloc]
+                 initWithFrequency:440
+                 amplitude:amplitude];
+    [generator play];
 }
 
 RCT_EXPORT_METHOD(stop)
 {
-    // TODO: Implement some real useful functionality
-    //callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+    [generator stop];
 }
 
 @end
